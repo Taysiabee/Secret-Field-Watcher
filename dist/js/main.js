82e8256412305e1be0dae009c7171abeab1e6ee5
Taysiabee <50152275+Taysiabee@users.noreply.github.com>
2020-05-21 14:40:11 -0400

    finished

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+new SecretFieldWatcher,new SecretFieldWatcher({name:"full_name",value:"Stella"}),new SecretFieldWatcher({name:"full_name",value:"Taco",foregroundColor:"black",backgroundColor:"pink",errorMessage:"Found You!"}),new SecretFieldWatcher({name:"email",value:"test@test.com",backgroundColor:"green",errorMessage:"Invalid Email"}),new SecretFieldWatcher({name:"full_name",value:"Porter",backgroundColor:"brown"});
+//# sourceMappingURL=main.js.map
